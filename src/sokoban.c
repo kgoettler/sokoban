d812a2810ff705b3c0d16cf5d1bd8ff2e9f37b5c
kgoettler <38511448+kgoettler@users.noreply.github.com>
2020-05-01 21:25:03 -0400

    Working version with a drawn map - currently just for looks\!

--- src/sokoban.c
+++ src/sokoban.c
@@ -90,8 +90,6 @@ void draw_map(sokoban_t * game)
     /* Determine where to start drawing */
     left = (COLS - game->width) / 2;
     top = (LINES - game->height) / 2;
-    printf("%d %d %d %d\n", game->width, game->height, left, top);
-    /*
     for (i = 0; i < game->width * game->height; i++)
     {
         x = left + (i % game->width);
@@ -100,7 +98,7 @@ void draw_map(sokoban_t * game)
         switch (game->map[i])
         {
             case OPEN:
-                mvaddstr(y, x, " ");
+                /*mvaddstr(y, x, " ");*/
                 break;
             case WALL:
                 mvaddstr(y, x, "#");
@@ -113,7 +111,6 @@ void draw_map(sokoban_t * game)
                 break; 
         }
     }
-    */
     return;
 }
 
@@ -130,99 +127,99 @@ int main (void)
     sokoban_t game;
     int key, res;
     
-    ///* Initialize ncurses session */
-    //initscr();
-    ///* Do not echo character input to screen */
-    //noecho();
-    ///* Hide cursor */
-    //curs_set(0);
-    ///* Accept function keys/arrow keys */
-    //keypad(stdscr, TRUE);
-    //
-    ///* Assign variables */
-    //game.player.x = COLS / 2;
-    //game.player.y = LINES / 2;
-    //game.box.x = COLS / 2 + 1;
-    //game.box.y = LINES / 2;
-    //key = 0;
+    /* Initialize ncurses session */
+    initscr();
+    /* Do not echo character input to screen */
+    noecho();
+    /* Hide cursor */
+    curs_set(0);
+    /* Accept function keys/arrow keys */
+    keypad(stdscr, TRUE);
+    
+    /* Assign variables */
+    game.player.x = COLS / 2;
+    game.player.y = LINES / 2;
+    game.box.x = COLS / 2 + 1;
+    game.box.y = LINES / 2;
+    key = 0;
 
     /* Read map */
     res = read_map(&game, map);
-    //draw_map(&game);
+    draw_map(&game);
 
-    ///* Draw + wait for input */
-    //mvaddstr(game.player.y, game.player.x, "o");
-    //mvaddstr(game.box.y, game.box.x, "@");
-    ///* Process input */
-    //while ((key = getch()) != 'q')
-    //{
-    //    /* Change game.player position */
-    //    switch (key)
-    //    {
-    //        case ('h'):
-    //        case (KEY_LEFT):
-    //            move_left(&game.player);
-    //            if (same_position(&game.player, &game.box) == 0)
-    //            {
-    //                if (game.player.x != 0)
-    //                    move_left(&game.box);
-    //                else
-    //                    move_right(&game.player);
-    //            }
-    //            break;
-    //        case ('l'):
-    //        case (KEY_RIGHT):
-    //            move_right(&game.player);
-    //            if (same_position(&game.player, &game.box) == 0)
-    //            {
-    //                if (game.player.x != COLS - 1)
-    //                    move_right(&game.box);
-    //                else
-    //                    move_left(&game.player);
-    //            }
-    //            break;
-    //        case ('k'):
-    //        case (KEY_UP):
-    //            move_up(&game.player);
-    //            if (same_position(&game.player, &game.box) == 0)
-    //            {
-    //                if (game.player.y != 0)
-    //                    move_up(&game.box);
-    //                else
-    //                    move_down(&game.player);
-    //            }
-    //            break;
-    //        case ('j'):
-    //        case (KEY_DOWN):
-    //            move_down(&game.player);
-    //            if (same_position(&game.player, &game.box) == 0)
-    //            {
-    //                if (game.player.y != LINES - 1)
-    //                    move_down(&game.box);
-    //                else
-    //                    move_up(&game.player);
-    //            }
-    //            break;
-    //        case ('r'):
-    //            game.player.x = COLS / 2;
-    //            game.player.y = LINES / 2;
-    //            game.box.x = game.player.x + 1;
-    //            game.box.y = game.player.y;
-    //        default:
-    //            break;
-    //    }
-    //    /* Check x,y */
-    //    check_boundary(&game.player);
+    /* Draw + wait for input */
+    mvaddstr(game.player.y, game.player.x, "o");
+    mvaddstr(game.box.y, game.box.x, "@");
+    /* Process input */
+    while ((key = getch()) != 'q')
+    {
+        /* Change game.player position */
+        switch (key)
+        {
+            case ('h'):
+            case (KEY_LEFT):
+                move_left(&game.player);
+                if (same_position(&game.player, &game.box) == 0)
+                {
+                    if (game.player.x != 0)
+                        move_left(&game.box);
+                    else
+                        move_right(&game.player);
+                }
+                break;
+            case ('l'):
+            case (KEY_RIGHT):
+                move_right(&game.player);
+                if (same_position(&game.player, &game.box) == 0)
+                {
+                    if (game.player.x != COLS - 1)
+                        move_right(&game.box);
+                    else
+                        move_left(&game.player);
+                }
+                break;
+            case ('k'):
+            case (KEY_UP):
+                move_up(&game.player);
+                if (same_position(&game.player, &game.box) == 0)
+                {
+                    if (game.player.y != 0)
+                        move_up(&game.box);
+                    else
+                        move_down(&game.player);
+                }
+                break;
+            case ('j'):
+            case (KEY_DOWN):
+                move_down(&game.player);
+                if (same_position(&game.player, &game.box) == 0)
+                {
+                    if (game.player.y != LINES - 1)
+                        move_down(&game.box);
+                    else
+                        move_up(&game.player);
+                }
+                break;
+            case ('r'):
+                game.player.x = COLS / 2;
+                game.player.y = LINES / 2;
+                game.box.x = game.player.x + 1;
+                game.box.y = game.player.y;
+            default:
+                break;
+        }
+        /* Check x,y */
+        check_boundary(&game.player);
 
-    //    /* Redraw */
-    //    clear();
-    //    mvprintw(game.player.y, game.player.x, "o");
-    //    mvprintw(game.box.y, game.box.x, "@");
-    //}
-    ///* Free up variables */
-    //endwin();
+        /* Redraw */
+        clear();
+        mvprintw(game.player.y, game.player.x, "o");
+        mvprintw(game.box.y, game.box.x, "@");
+        draw_map(&game);
+    }
+    /* Free up variables */
     free(game.map);
-    //endwin();
+    endwin();
     return 0;
 }
 
